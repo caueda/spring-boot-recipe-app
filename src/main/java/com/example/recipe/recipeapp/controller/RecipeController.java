b7f2b49a645458c33a8476908a1aea8586be535d
carlos.ueda <carlos.ueda@gmail.com>
2020-10-08 11:40:01 -0400

    Swagger configured in the project. Closes #5

--- src/main/java/com/example/recipe/recipeapp/controller/RecipeController.java
+++ src/main/java/com/example/recipe/recipeapp/controller/RecipeController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.recipe.recipeapp.domain.Recipe;
 import com.example.recipe.recipeapp.service.RecipeService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Tag;
+
 @Controller
 @RequestMapping("/api/v1/recipe")
 public class RecipeController {
@@ -21,11 +24,13 @@ public class RecipeController {
 		this.recipeService = recipeService;
 	}
 	
+	@ApiOperation(value = "This will get a list of recipes")
 	@GetMapping
 	public ResponseEntity<List<Recipe>> findAll() {
 		return new ResponseEntity<List<Recipe>> (recipeService.findAll(), HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "This will get a specific recipe by its identification id")
 	@GetMapping("/{id}")
 	public ResponseEntity<Recipe> findById(@PathVariable String id) {
 		return new ResponseEntity<Recipe>(recipeService.findById(id).get(), HttpStatus.OK);
