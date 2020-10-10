9d0376a001071b5626a607be7d78871ee8857798
carlos.ueda <carlos.ueda@gmail.com>
2020-10-09 22:25:04 -0400

    Bug -  Recipe search by id. Closes #9

--- src/main/java/com/example/recipe/recipeapp/controller/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/recipe/recipeapp/controller/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.recipe.recipeapp.controller.exceptions;
 
 import java.util.Date;
+import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,5 +24,16 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 						.detail(webRequest.getDescription(false))
 						.build();
 			return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		@ExceptionHandler(NoSuchElementException.class)
+		public final ResponseEntity<Object> handleNoSuchElementException(Exception ex, WebRequest webRequest) {
+			ExceptionResponse exceptionResponse =
+					ExceptionResponse.builder()
+						.timestamp(new Date())
+						.message(ex.getMessage())
+						.detail(webRequest.getDescription(false))
+						.build();
+			return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 		}
 }
